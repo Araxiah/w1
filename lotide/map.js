18b99159f1f39ea9cce5553442427ebab9d7260d
Carol Woo <cewoo98@hotmail.com>
2019-09-19 19:22:38 +0000

    finished map.js

---
+++ lotide/map.js
@@ -0,0 +1,18 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
+}
+
+const callback = function (i) {
+  return i[0]
+}
+console.log(map(words, callback))
